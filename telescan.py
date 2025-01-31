ce0d4a8ed37014b9241afc251638f7d39bf18881
Francesco Poldi <francesco.poldi@protonmail.ch>
2025-01-31 13:14:29 +0100

    Fix for #6: implemented configparser with previous logic for backward compatibility

--- telescan.py
+++ telescan.py
@@ -1,15 +1,9 @@
-import time, sys
+import time, sys, configparser
 
 from colorama import init, Fore, Style
 from pyrogram import Client
 from pyrogram.errors import BadRequest, FloodWait, UnknownError
 from tqdm import tqdm
-
-cFile = sys.argv[1]
-app = Client(
-    config_file=cFile,
-    session_name=cFile.split('.')[0]
-)
 
 init(autoreset=True)
 print(Fore.CYAN + """
@@ -22,36 +16,51 @@ print(Fore.CYAN + """
 --- v1.0 by @Pielco11 ---
 """)
 
+def loadConfig():
+    cFile = ""
+    if len(sys.argv) > 1:
+        cFile = sys.argv[1]
+    else:
+        return {}
+    config = configparser.ConfigParser()
+    config.read(cFile)
+    
+    return {
+        "api_id": int(config.get("pyrogram", "api_id")),
+        "api_hash": config.get("pyrogram", "api_hash"),
+        "session_name": cFile.split(".")[0]
+    }
+
 def userID(userInput):
     if userInput.startswith("pn-") or userInput.startswith("u-"):
-        return userInput.split('-')[1]
+        return userInput.split("-")[1]
     elif userInput.startswith("id-"):
-        return int(userInput.split('-')[1])
+        return int(userInput.split("-")[1])
     else:
         print(Fore.RED + "[x] please specify a correct prefix (id-/u-/pn-). Exiting!" + Style.RESET_ALL)
         exit()
 
 
 def chatListPrint(data):
-    _id = data['id']
-    _type = data['type']
-    _title = data['title']
-    _dc = data['dc_id']
-    _username = data['username']
+    _id = data["id"]
+    _type = data["type"]
+    _title = data["title"]
+    _dc = data["dc_id"]
+    _username = data["username"]
     print("[id] {} | Title: {} | Username: {} | Type: {} | DC: {}".format(_id, _title, _username, _type, _dc))
 
 def singleUserLookup(user):
-    _id = user['user']['id']
-    _contact = user['user']['is_contact']
-    _first_name = user['user']['first_name']
-    _last_name = user['user']['last_name']
+    _id = user["user"]["id"]
+    _contact = user["user"]["is_contact"]
+    _first_name = user["user"]["first_name"]
+    _last_name = user["user"]["last_name"]
     _fullName = _first_name + " " + _last_name if _last_name else _first_name
-    _username = user['user']['username']
-    _phone = user['user']['phone_number']
-    _dc = user['user']['dc_id']
-    _lod = user['user']['last_online_date']
+    _username = user["user"]["username"]
+    _phone = user["user"]["phone_number"]
+    _dc = user["user"]["dc_id"]
+    _lod = user["user"]["last_online_date"]
     if _lod:
-        _lod = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(_lod))
+        _lod = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(_lod))
     print("------\n|-> ID: {}\n|-> Contact: {}\n|-> Full name: {}\n|-> Username: {}\n|-> Phone: {}".format(_id, _contact, _fullName, _username, _phone))
     print("|-> DC: {}\n|-> Last Online Date: {}".format(_dc, _lod))
 
@@ -67,17 +76,17 @@ def chatMembersInfoPrint(data, total=True):
     print(Fore.GREEN + "[user infos]" + Style.RESET_ALL)
     for user in userList:
         singleUserLookup(user)
-        _status = user['status']
+        _status = user["status"]
         _date = None
         try:
-            _date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(user['joined_date']))
+            _date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(user["joined_date"]))
         except AttributeError:
             pass
-        if user['invited_by']:
-            _invitedByID = user['invited_by']['id']
-            _invitedByUsername = user['invited_by']['username']
-            _invitedByFirstName = user['invited_by']['first_name']
-            _invitedBySurname = user['invited_by']['last_name']
+        if user["invited_by"]:
+            _invitedByID = user["invited_by"]["id"]
+            _invitedByUsername = user["invited_by"]["username"]
+            _invitedByFirstName = user["invited_by"]["first_name"]
+            _invitedBySurname = user["invited_by"]["last_name"]
             _invitedByFullName = _invitedByFirstName + " " + _invitedBySurname if _invitedBySurname else _invitedByFirstName
         else:
             _invitedByID = 0
@@ -86,6 +95,14 @@ def chatMembersInfoPrint(data, total=True):
         print("|-> Status: {}\n|-> Join date: {}".format(_status, _date))
         print("|-> Invited by id: {} | username: {} | Full name: {}".format(_invitedByID, _invitedByUsername, _invitedByFullName))
 
+config = loadConfig()
+if not config:
+    exit("[x] first argument must be a .ini config file; exiting.")
+app = Client(
+    config["session_name"],
+    api_id=config["api_id"],
+    api_hash=config["api_hash"]
+)
 
 with app:
     while True:
@@ -97,12 +114,12 @@ with app:
             dialogs = app.get_dialogs()
             i = 0
             for d in dialogs:
-                chatListPrint(d['chat'])
+                chatListPrint(d["chat"])
                 i += 1
                 if i >= 10:
                     cmd = input(Fore.CYAN + "[(c)/x]: " + Style.RESET_ALL)
                     i = 0
-                    if cmd == 'x':
+                    if cmd == "x":
                         break
             chatID = input(Fore.CYAN + "[chat lookup]: " + Style.RESET_ALL)
             choice = input(Fore.CYAN + "[1] => Bulk search\n[2] => single user lookup\n[<]: " + Style.RESET_ALL)
@@ -117,16 +134,16 @@ with app:
         elif choice == "2":
             userInput = input(Fore.CYAN + "[user-id (id)/username (u)/phone number (pn)]: " + Style.RESET_ALL)
             lookupResult = app.get_users([userID(userInput)])
-            singleUserLookup({'user': lookupResult[0]})
+            singleUserLookup({"user": lookupResult[0]})
         elif choice == "3":
             dialogs = app.get_dialogs()
             userInput = input(Fore.CYAN + "[user-id (id)/username (u)/phone number (pn)]: " + Style.RESET_ALL)
             for d in dialogs:
-                if int(d['chat']['id']) < 0:
+                if int(d["chat"]["id"]) < 0:
                     try:
-                        members = app.get_chat_member(chat_id=int(d['chat']['id']), user_id=userID(userInput))
+                        members = app.get_chat_member(chat_id=int(d["chat"]["id"]), user_id=userID(userInput))
                         if members:
-                            chatListPrint(d['chat'])
+                            chatListPrint(d["chat"])
                     except BadRequest as e:
                         if not ("CHAT_ADMIN_REQUIRED" or "USER_NOT_PARTICIPANT" in str(e)):
                             print(str(e))
